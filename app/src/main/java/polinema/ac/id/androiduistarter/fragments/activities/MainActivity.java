585f815325f7217aa2c6076f9f31bf936d83741b
firnandakusuma <firnandapratama99@gmail.com>
2019-10-07 15:24:11 +0700

    Bottom Navbar

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/MainActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/MainActivity.java
@@ -1,16 +1,46 @@
 package polinema.ac.id.androiduistarter.fragments.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import polinema.ac.id.androiduistarter.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import polinema.ac.id.androiduistarter.*;
+import polinema.ac.id.androiduistarter.fragments.fragments.FoodFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new FoodFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
